b578005ef496289cbed87733a3bda6b8c001084e
Cole Horvat <colehorvat@pop-os.localdomain>
2022-01-27 17:14:51 +0000

    Got sizing correct

--- Wearabouts/index.js
+++ Wearabouts/index.js
@@ -1,15 +1,37 @@
+/* Get options, colors, sizes*/
+
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const url = 'http://127.0.0.1:5500/Wearabouts/dresses-test.html'
-const descriptionUrl = 'http://127.0.0.1:5500/Wearabouts/dresses-description-test.html'
+const descriptionUrl = 'https://wearabouts.ca/product/fig-ws-etosha-dress/'
 
-async function getHtml() {
-    const htmlResult = await request.get(descriptionUrl);
-    const $ = await cheerio.load(htmlResult)
+const colors = [];
+const sizes = []
 
-    console.log(htmlResult);
+async function scrapeProductPage() {
+    const html = await request.get(descriptionUrl);
+    const $ = await cheerio.load(html);
+    await sleep(1000);
+
+    const colorElement = $("#pa_color");
+    const sizeElement = $("#pa_size option");
+
+    sizeElement.each((index, element) => {
+        if(index !== 0) {
+            const size = $(element).text();
+
+            sizes.push(size);
+        }
+    });
+    console.log(sizes)
 }
 
-getHtml();
+async function sleep(miliseconds)
+{
+    return new Promise(resolve => setTimeout(resolve,miliseconds));
+}
+
+
+scrapeProductPage();
 

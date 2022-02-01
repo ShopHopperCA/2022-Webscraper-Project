2ecc4b6a2bc337c009dabe1770ef92110263cf6c
Cole Horvat <colehorvat@pop-os.localdomain>
2022-02-01 21:16:05 +0000

    Found out but, downgraded node-fetch to v2

--- lightspeed-api/index.js
+++ lightspeed-api/index.js
@@ -1,2 +1,28 @@
-const request = require('request-promise');
+const fetch = require('node-fetch');
+const request = require('request-promise')
 const cheerio = require('cheerio');
+
+const fossellos = require('./fosellos_products');
+
+
+const data = {};
+const product_titles = []
+
+const fossellos_urls = fossellos.scrapeProductTitles();
+
+async function main() {
+    console.log(await fossellos_urls);
+}
+
+/* UTILITY FUNCTIONS */
+
+//Stops the program for a specified number of seconds
+async function sleep(miliseconds)
+{
+    return new Promise(resolve => setTimeout(resolve,miliseconds));
+}
+
+main();
+
+
+

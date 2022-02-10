1a51d27a9aa43d0307b8d103787141a56439af84
Cole Horvat <colehorvat@pop-os.localdomain>
2022-02-10 17:10:08 +0000

    Uncommented site objects

--- lightspeed-api/site_objects.js
+++ lightspeed-api/site_objects.js
@@ -12,27 +12,27 @@ const SITE_OBJECTS = [
         productLinkSelector : ".product-image-wrapper",
     },
 
-//     RED_TOP_FOOTWEAR = {
-//         baseUrl : "https://red-top-footwear.shoplightspeed.com/ladies/",
-//         paginationSelector : "ul.right > li", 
-//         productListSelector : "div.col-md-9", 
-//         productLinkSelector : "a.title",
-//         removeNodes : [".clearfix",]
-//     },
+    RED_TOP_FOOTWEAR = {
+        baseUrl : "https://red-top-footwear.shoplightspeed.com/ladies/",
+        paginationSelector : "ul.right > li", 
+        productListSelector : "div.col-md-9", 
+        productLinkSelector : "a.title",
+        removeNodes : [".clearfix",]
+    },
 
-//     ENVY_APPAREL = {
-//         baseUrl : "https://shop.envyapparelfit.com/shop/",
-//         paginationSelector : "div.pagination > ul > li", 
-//         productListSelector : "div.products-list", 
-//         productLinkSelector : ".product-image-wrapper",
-//     },
+    ENVY_APPAREL = {
+        baseUrl : "https://shop.envyapparelfit.com/shop/",
+        paginationSelector : "div.pagination > ul > li", 
+        productListSelector : "div.products-list", 
+        productLinkSelector : ".product-image-wrapper",
+    },
 
-//     HONEST_BOUTIQUE = {
-//         baseUrl : "https://www.honestboutique.ca/clothing/",
-//         paginationSelector : "div.pagination > ul > li", 
-//         productListSelector : "div.products-list", 
-//         productLinkSelector : ".product-image-wrapper",
-//     }
+    HONEST_BOUTIQUE = {
+        baseUrl : "https://www.honestboutique.ca/clothing/",
+        paginationSelector : "div.pagination > ul > li", 
+        productListSelector : "div.products-list", 
+        productLinkSelector : ".product-image-wrapper",
+    }
 ]
 
 module.exports = { SITE_OBJECTS }
